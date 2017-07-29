61d1380f7819e936587c8742798200cbc23a81c2
Amol Kulkarni <amol.kulkarni@synerzip.com>
2017-07-29 22:07:43 +0530

    Added mail reply.

--- controllers/mail.js
+++ controllers/mail.js
@@ -19,14 +19,26 @@ module.exports = {
                 '</i></p><br><br><b>You can contact </b>  <a href=' + req.body.email + '>' + req.body.email +'</a> for more details'// You can choose to send an HTML body instead
         };
 
+        var replyOptions = {
+            from: 'abcmontessoricontact@gmail.com', // sender address
+            subject: 'Thanks for Enquiry!',
+            to: req.body.email,
+            html: '<b>Dear ' + req.body.name + ',</b><br>'+
+               '<p>Thanks for showing interest.</p><p>We will get back to you shortly.</p><br>'+
+               '<i> - Kids Academy</i>'
+        }
+
         transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
                 console.log(error);
+                transporter.sendMail(replyOptions);
                 res.json({ error: error });
             } else {
                 console.log('Message sent: ' + info.response);
+                transporter.sendMail(replyOptions);
                 res.json("success");
             };
         });
+
     }
 };
